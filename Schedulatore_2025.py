c7c90f916e38dd2db9242242d5f38084e3d2ff46
alebelluco <148531503+alebelluco@users.noreply.github.com>
2025-07-23 09:41:24 +0200

    Update Schedulatore_2025.py

--- Schedulatore_2025.py
+++ Schedulatore_2025.py
@@ -156,7 +156,7 @@ with tab4:
     df_raw = df_raw[df_raw.Cliente != 'FERRARA TUA SPA']
     df_raw = df_raw[df_raw['Operatore'] != ' (FAT) (FAT)']
 
-   if st.toggle('Rimuovi zanzare'):
+    if st.toggle('Rimuovi zanzare'):
         df_raw = df_raw[['ANTILARVALE' not in check for check in df_raw.Servizio]]
         df_raw = df_raw[['ANTIADULTO' not in check for check in df_raw.Servizio]]
 
