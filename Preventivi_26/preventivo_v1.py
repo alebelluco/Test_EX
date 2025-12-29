1644f61fe67e98b5d108dcd8d417be9bddda94e5
alebelluco <148531503+alebelluco@users.noreply.github.com>
2025-12-29 16:33:12 +0100

    Fix template path for Word document generation
    Updated template path to include raw parameter for direct access.

--- Preventivi_26/preventivo_v1.py
+++ Preventivi_26/preventivo_v1.py
@@ -526,7 +526,7 @@ with t_riep:
 
         # Template path
         #template_path = 'modellonew.docx' 
-        template_path = 'https://github.com/alebelluco/Test_EX/blob/main/Preventivi_26/modellonew.docx'
+        template_path = 'https://github.com/alebelluco/Test_EX/blob/main/Preventivi_26/modellonew.docx?raw=True'
         
         if st.button('Genera Documento Word'):
 
