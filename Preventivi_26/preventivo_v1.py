23557a4d3744fcce7f661445488a688e1c984db3
alebelluco <148531503+alebelluco@users.noreply.github.com>
2025-12-29 16:36:06 +0100

    Update template path for document generation

--- Preventivi_26/preventivo_v1.py
+++ Preventivi_26/preventivo_v1.py
@@ -526,7 +526,7 @@ with t_riep:
 
         # Template path
         #template_path = 'modellonew.docx' 
-        template_path = 'https://github.com/alebelluco/Test_EX/blob/main/Preventivi_26/modellonew.docx?raw=True'
+        template_path = 'https://raw.githubusercontent.com/alebelluco/Test_EX/main/Preventivi_26/modellonew.docx'
         
         if st.button('Genera Documento Word'):
 
